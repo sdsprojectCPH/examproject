6d558e90e9766ff6fd864a985b487c433ff63b5f
JanekEskildsen <janekeskildsen@gmail.com>
2016-08-22 09:26:46 +0200

    Add files via upload
    This code is for downloading location of listings

---
+++ Downloading location.R
@@ -0,0 +1,41 @@
+
+if(!require("readr")) install.packages("readr")
+if(!require("plyr")) install.packages("plyr")
+if(!require("dply")) install.packages("dplyr")
+if(!require("tidyr")) install.packages("tidyr")
+if(!require("ggmap")) install.packages("ggmap")
+
+library("readr")
+library("plyr")
+library("dplyr")
+library("tidyr")
+library("ggmap")
+
+getwd()
+setwd("Desktop")
+
+url.listings = "http://data.insideairbnb.com/denmark/hovedstaden/copenhagen/2016-06-28/data/listings.csv.gz"
+df.listings = read_csv(url.listings)  #Downloads listings into data frame
+
+df.location = df.listings %>%
+  mutate(
+    lat = as.numeric(latitude),
+    lon = as.numeric(longitude),
+    zip = as.numeric(zipcode)
+  ) %>% 
+  select(lon, lat, zip, id) %>% 
+  arrange(-zip)
+
+df.location1 = df.location[1:2500, ]
+
+result = do.call(rbind,
+                 lapply(1:nrow(df.location),
+                        function(i)revgeocode(as.numeric(df.location[i,1:2]))))
+
+df.location_missing1 = cbind(df.location,result)
+
+library(stringr)
+df.location_missing1$zipcode <- substring(str_extract(df.location_missing$result,", [0-9]{4} "),2,8)
+df.zipcode1 = cbind(df.location,df.location_missing$zipcode)
+
+write.csv(file = "zipcodes1.csv", x = df.location_missing1)
